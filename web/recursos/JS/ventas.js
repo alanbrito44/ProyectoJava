b5c6d2336c6d30b5d53bfd9a7717844019fb7ac2
Javier Amaya <Javier Amaya@DESKTOP-6M2D19A>
2021-11-12 00:55:52 -0600

    Funcionando Seleccion de Automoviles  y haciendo correcta la operacion de cantidad por precio!

--- web/recursos/JS/ventas.js
+++ web/recursos/JS/ventas.js
@@ -19,19 +19,35 @@ $(document).ready(function(){
         $('#mdlVehiculos').modal('hide');
    })
    
+   $('body').on('change', '#cantidad', function (){
+       let fila = $(this).closest('tr');
+       
+       var precio = fila.find('#precio').text();
+       var cantidad = fila.find('#cantidad').val();
+       var subTotal = precio * cantidad;
+       fila.find('#subTotal').text(subTotal);
+       
+       
+       
+       
+     
+   })
+   
     function llamarCarros(fila){
         var codigo = fila.find('.id_auto').text();
         var marca = fila.find('.marca_auto').text();
         var modelo = fila.find('.modelo_auto').text();
         var precio = fila.find('.precio_auto').text();
+        var subTotal = precio * 1; 
+        
         
         var tr = '<tr>'+
          '<td>'+codigo+'</td>'+
          '<td>'+marca+'</td>'+
          '<td>'+modelo+'</td>'+
-         '<td>'+precio+'</td>'+
-         '<td><input type="number" value="1" style=" width: 120px;" id="Cantidad" class="form-control" name="txtCantidad"></td>'+         
-         '<td>'+precio*($('#Cantidad').val())+'</td>'+
+         '<td id="precio">'+precio+'</td>'+
+         '<td><input type="number" min="0" value="1" style=" width: 120px;" id="cantidad" class="form-control" name="txtCantidad"></td>'+                
+         '<td id="subTotal">'+subTotal+'</td>'+
          '<td>'+
               '<button type="button" class="btn btn-warning btn-sm"  id="btnBorrar">'+
                 'Eliminar'+
@@ -40,6 +56,8 @@ $(document).ready(function(){
                 '</tr>';
  
          $('#facturacion').append(tr);
+    
+      
     }
     function llamarDatos(fila){
         var codigo = fila.find('.codigo').text();
